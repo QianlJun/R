54dab3462ccc86fe334f33ab148dc9e089a1281b
QianlJun <41985954+QianlJun@users.noreply.github.com>
2019-08-27 22:12:54 +0800

    delete

--- Rcode/Biostatistics/Binomial Proportions Part B.R
+++
@@ -1,3 +0,0 @@
-install.packages(binom)
-library(binom)
-binom.bayes(13,20,type="highest")
--- Rcode/Biostatistics/Limits and LLN.R
+++
@@ -1,4 +0,0 @@
-nosim<-1000
-x<-cumsum(rnorm(nosim))/1:nosim
-plot(1:nosim,x,type="l",xlab="Iteration",ylab="Average",frame=FALSE)
-abline(h=0,lty=2)
